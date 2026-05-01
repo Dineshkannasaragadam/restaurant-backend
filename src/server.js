5038033f2b916ffc59b5de908825ca01f9f593e6
Dineshkannasaragadam <143860624+Dineshkannasaragadam@users.noreply.github.com>
2026-05-01 15:03:47 +0530

    Change server port from 5001 to 5000

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const { connectDB } = require('./config/database');
 const { initSocket } = require('./config/socket');
 const logger = require('./utils/logger');
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Create HTTP server
 const server = http.createServer(app);
